0b75ebd1bf7d757a5bf2c4178968c93b9040d198
Dawid Taborski <dtaborski@apptio.com>
2023-05-15 13:36:58 +0200

    Dodanie podgladu produktu

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/Link'
 import Title from "@/components/Title";
-import {useState} from "react";
 import {getProducts} from "@/lib/products";
 
-// This function it gets callend during building the application
+// This function it gets called during building the application
 export async function getStaticProps() {
   const products = await getProducts();
   return {
@@ -14,52 +14,28 @@ export async function getStaticProps() {
     // so it is NOT refetched on every page load !!! to mitigate that we need
     // revalidate: 60seconds means that the page will be revalidated every 60 seconds
     // this is called ISR
-    revalidate: 5 * 60, // seconds
+    revalidate: 30, // seconds
   }
 }
 
-// to make our application refetch data on every page load we need to use getServerSideProps
-// server side fetch passed as a props to component!!
-// export async function getServerSideProps() {
-//   const products = await getProducts();
-//   return {
-//     props: {
-//       products,
-//     },
-//   }
-// }
-
-export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  // fetching CMS directly
-  // useEffect(() => {
-  //   getProducts().then(setProducts);
-  // }, [])
-  // fetching api trough proxy defined in api/products.js
-  // useEffect(() => {
-  //   (async () => {
-  //     const res = await fetch("/api/products");
-  //     const products = await res.json();
-  //     setProducts(products);
-  //   })();
-  // }, [])
-
+export default function Home({products}) {
   return (
-      <>
-        <Head>
-          <title>Next Shop</title>
-        </Head>
-        <main className="p-2">
-          <Title>Next Shop</Title>
-          <ul>
-            {products.map((product) => (
-                <li key={product.id}>
-                  {product.title}
-                </li>
-            ))}
-          </ul>
-        </main>
-      </>
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className="p-2">
+        <Title>Next Shop</Title>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <Link href={`products/${product.id}`}>
+                {product.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   )
 }
---
+++ pages/products/[id].js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Title from "@/components/Title";
+import {getProduct, getProducts} from "@/lib/products";
+
+export async function getStaticPaths() {
+  const products = await getProducts()
+  return {
+    paths: products.map((product) => ({
+      params: {id: product.id.toString()}
+    })),
+    fallback: false,
+    revalidate: 30,
+  }
+}
+
+export async function getStaticProps({params}) {
+  const product = await getProduct(params.id)
+
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+const Product = ({product}) => {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className="p-2">
+        <Title>{product.title}</Title>
+        <p>{product.description}</p>
+      </main>
+    </>
+  );
+};
+
+export default Product;
